107ad26de2d9eeb9fda7e1a926f8d42ae44b8b81
vlads1995 <vlad_s1995@mail.ru>
2019-03-31 18:28:52 +0300

    delete from master

--- Scripts/Interfaces/IDamagable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamagable<T>
-{
-    int Health { get; set; }
-    void Damage(T damageAmount);
-}
